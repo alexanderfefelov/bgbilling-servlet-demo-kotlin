84c85cf30732f71c88256fbe03aff097f5a4f6c6
Alexander Fefelov <alexanderfefelov@yandex.ru>
2019-01-19 06:56:35 +0300

    Refactor

--- src/main/kotlin/com/github/alexanderfefelov/bgbilling/servlet/demo/DemoServletKotlin.kt
+++ src/main/kotlin/com/github/alexanderfefelov/bgbilling/servlet/demo/DemoServletKotlin.kt
@@ -9,91 +9,55 @@ import javax.servlet.http.HttpServletResponse
 
 class DemoServletKotlin : HttpServlet() {
 
-    override fun init() {
-        try {
-            NestedContext.push(LOG_CONTEXT)
-            logger.trace("init")
-        } finally {
-            NestedContext.pop()
-        }
+    override fun init() = wrap {
+        logger.trace("init")
     }
 
-    override fun destroy() {
-        try {
-            NestedContext.push(LOG_CONTEXT)
-            logger.trace("destroy")
-        } finally {
-            NestedContext.pop()
-        }
+    override fun destroy() = wrap {
+        logger.trace("destroy")
     }
 
-    override fun doGet(request: HttpServletRequest, response: HttpServletResponse) {
-        try {
-            NestedContext.push(LOG_CONTEXT)
-            logger.trace("doGet")
-            val kernelVer = VersionInfo.getVersionInfo("server")
-            response.writer.println("Hello, World!")
-            response.writer.println(kernelVer.getModuleName() + " " + kernelVer.getVersionString())
-        } finally {
-            NestedContext.pop()
-        }
+    override fun doGet(request: HttpServletRequest, response: HttpServletResponse) = wrap {
+        logger.trace("doGet")
+        val kernelVer = VersionInfo.getVersionInfo("server")
+        response.writer.println("Hello, World!")
+        response.writer.println(kernelVer.getModuleName() + " " + kernelVer.getVersionString())
     }
 
-    override fun doPost(request: HttpServletRequest, response: HttpServletResponse) {
-        try {
-            NestedContext.push(LOG_CONTEXT)
-            logger.trace("doPost")
-            super.doPost(request, response)
-        } finally {
-            NestedContext.pop()
-        }
+    override fun doPost(request: HttpServletRequest, response: HttpServletResponse) = wrap {
+        logger.trace("doPost")
+        super.doPost(request, response)
     }
 
-    override fun doPut(request: HttpServletRequest, response: HttpServletResponse) {
-        try {
-            NestedContext.push(LOG_CONTEXT)
-            logger.trace("doPut")
-            super.doPut(request, response)
-        } finally {
-            NestedContext.pop()
-        }
+    override fun doPut(request: HttpServletRequest, response: HttpServletResponse) = wrap {
+        logger.trace("doPut")
+        super.doPut(request, response)
     }
 
-    override fun doDelete(request: HttpServletRequest, response: HttpServletResponse) {
-        try {
-            NestedContext.push(LOG_CONTEXT)
-            logger.trace("doDelete")
-            super.doDelete(request, response)
-        } finally {
-            NestedContext.pop()
-        }
+    override fun doDelete(request: HttpServletRequest, response: HttpServletResponse) = wrap {
+        logger.trace("doDelete")
+        super.doDelete(request, response)
     }
 
-    override fun doHead(request: HttpServletRequest, response: HttpServletResponse) {
-        try {
-            NestedContext.push(LOG_CONTEXT)
-            logger.trace("doHead")
-            super.doHead(request, response)
-        } finally {
-            NestedContext.pop()
-        }
+    override fun doHead(request: HttpServletRequest, response: HttpServletResponse) = wrap {
+        logger.trace("doHead")
+        super.doHead(request, response)
     }
 
-    override fun doOptions(request: HttpServletRequest, response: HttpServletResponse) {
-        try {
-            NestedContext.push(LOG_CONTEXT)
-            logger.trace("doOptions")
-            super.doOptions(request, response)
-        } finally {
-            NestedContext.pop()
-        }
+    override fun doOptions(request: HttpServletRequest, response: HttpServletResponse) = wrap {
+        logger.trace("doOptions")
+        super.doOptions(request, response)
     }
 
-    override fun doTrace(request: HttpServletRequest, response: HttpServletResponse) {
+    override fun doTrace(request: HttpServletRequest, response: HttpServletResponse) = wrap {
+        logger.trace("doTrace")
+        super.doTrace(request, response)
+    }
+
+    private fun <R> wrap(block: () -> R) {
         try {
             NestedContext.push(LOG_CONTEXT)
-            logger.trace("doTrace")
-            super.doTrace(request, response)
+            block()
         } finally {
             NestedContext.pop()
         }
